0d99ce50159addd6efdea1797c9a48726bd8d9a7
Saurabh Ojha <ojha.saurabh81@gmail.com>
2025-02-20 16:27:05 +0530

    adding styling, header and form listing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { FormListing } from './components/FormListing/FormListing'
+import Header from './components/Header/Header'
 import './styles/App.css'
 
 function App() {
 
   return (
     <>
-
-      
+      <Header />
+      <FormListing data={[{
+        name: "Demo",
+        description: "First demo",
+        createdAt: "20 Feb 2025 14:00",
+        stats: {
+          fieldCount: 8,
+          usageCount: 100,
+        }
+      }]} />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import './styles/Index.css'
+import './styles/Colors.css'
+import './styles/Fonts.css'
+import './styles/Headings.css'
+import './styles/Buttons.css'
+import './styles/Inputs.css'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
