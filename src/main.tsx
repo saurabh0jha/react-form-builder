4d5bb53a059c3b3cba9aeeb318cbffc06a2ded7c
Saurabh Ojha <ojha.saurabh81@gmail.com>
2025-02-21 23:34:06 +0530

    fixing typescript errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/Index.css'
-import './styles/Colors.css'
-import './styles/Fonts.css'
-import './styles/Headings.css'
-import './styles/Buttons.css'
-import './styles/Inputs.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/App.css";
+import "./styles/Index.css";
+import "./styles/Colors.css";
+import "./styles/Fonts.css";
+import "./styles/Headings.css";
+import "./styles/Buttons.css";
+import "./styles/Inputs.css";
 
-import App from './App.tsx'
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
